21454f101a7a3f60bc6cbf414a215c14a26d58e3
Eric <eric2.0turcotte@gmail.com>
2024-02-13 13:08:14 -0500

    region create endpoint update

--- src/shared/db/mongodb/schemas/region.Schema.js
+++ src/shared/db/mongodb/schemas/region.Schema.js
@@ -1,28 +1,26 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const Agent = require('./agent.Schema')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Agent = require('./agent.Schema');
 
-const RegionSchema = new mongoose.Schema({
-    region: {
-        type: String,
-        enum:{
-            values:['north', 'south', 'east', 'west'],
-            message: '{VALUE} is not supported'
-        },
-        required: true
+const RegionSchema = new Schema({
+  region: {
+    type: String,
+    enum: {
+      values: ['north', 'south', 'east', 'west'],
+      message: '{VALUE} is not supported'
     },
-    address: {
-        type: String,
-        // trim: true,
-        required: true
-    },
-    total_sales: { 
-        type: Number,
-        min: 0
-    },
-    manager: [Agent.schema],
-    top_agents: [Agent.schema]
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  total_sales: {
+    type: Number,
+    min: 0
+  },
+  manager: [Agent.schema],
+  top_agents: [Agent.schema]
+}, { timestamps: true });
 
-}, { timestamps: true })
-
-module.exports = mongoose.model('Region', RegionSchema)
+module.exports = mongoose.model('Region', RegionSchema);
