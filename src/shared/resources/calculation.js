a0f36d322e5d7b392382010219790ddc453b7200
Eric <eric2.0turcotte@gmail.com>
2024-02-22 10:41:32 -0500

    clean up project

--- src/shared/resources/calculation.js
+++ src/shared/resources/calculation.js
@@ -1,16 +1,3 @@
-// IMPORT FROM DATA.JS
-// import { unitPrices, installPercentFees } from './data.js';
-//
-// export const calcResidentialElev = (floors, apts) => Math.ceil(apts / floors / 6) * Math.ceil(floors / 20);
-// export const calcCommercialElev = (floors, maxOccupancy) => (Math.ceil((maxOccupancy * floors) / 200) * Math.ceil(floors / 10)) + (Math.ceil(floors / 10));
-// export const calcIndustrialElev = (elevatorsNeeded) => elevatorsNeeded;
-//
-// export const calcInstallFee = (numElevators, tier) => {
-//   const UNIT_PRICE = unitPrices[tier];
-//   const INSTALL_PERCENT_FEE = installPercentFees[tier];
-//   return Math.round(numElevators * UNIT_PRICE * ((INSTALL_PERCENT_FEE / 100) + 1));
-// };
-// const { unitPrices, installPercentFees } = require('../../shared/resources/data');
 
 const data = require('./data');
 
